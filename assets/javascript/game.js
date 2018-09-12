1a63183b6893cebf4260e4f6315fa445c4b1e4dd
strikerst09 <andrewdehaas5988@gmail.com>
2018-09-12 17:37:07 -0500

    updated html and script, functional, really need css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,40 +3,38 @@
 // ----Variables---- //
 // Variable for target number
 var targetNumber = Math.floor(Math.random() * 101) + 19;
-document.getElementById("randomNumberBox").innerHTML = targetNumber;
+$("#randomNumberBox").text("Your target is: " + targetNumber);
 
 // Variables for user count, user wins, user losses
 var counter = 0;
 var wins = 0;
 var losses = 0;
 
-// Variable for crystal values
-var numberOptions = [1,2,7,12];
-
-// For statement assigns values to crystals
-// for (var i = 0; i < numberOptions.length; i++) {
-//     var imageCrystal = document.querySelectorAll(".crystal-image");
-//     imageCrystal.getAttribute("data-crystalvalue", numberOptions[i]);
-//     document.querySelectorAll(".crystal-image").join(imageCrystal);
-// }
-
-for (var i = 0; i < numberOptions.length; i++) {
-    var imageCrystal = $("<img>");
-    imageCrystal.addClass("crystal-image");
-    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-    $("#crystals").append(imageCrystal);
+// Function for setting crystal value
+document.onload() function() {
+    var crystalValue = 
 }
 
 // Function for on click event
 $(".crystal-image").on("click", function() {
     var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue - parseInt(crystalValue);
-    console.log(crystalValue);
+    crystalValue = parseInt(crystalValue);
     counter += crystalValue;
-    document.getElementById("userNumberBox").innerHTML = 
-    ("New total: " + counter);
+    $("#userNumberBox").text("Your current number is: " + counter); 
     if (counter === targetNumber) {
-        document.getElementById("totalsBox").innerHTML = ("You win!")
-    };
+        wins++;
+        $("#totalsBox").text("You win! Total Wins: " + wins + " Total Losses: " + losses)   
+    }
+    else if (counter >= targetNumber) {
+        losses++;
+        $("#totalsBox").text("You LOSE! Total Wins: " + wins + " Total Losses: " + losses);
+    }
+});
+
+//Function to reset target number and counter
+$("#reset").on("click", function() {
+    counter = 0;
+    targetNumber = Math.floor(Math.random() * 101) + 19;
+    $("#randomNumberBox").text("Your target is: " + targetNumber);
+    $("#userNumberBox").text("Your current number is: " + counter);
 });
