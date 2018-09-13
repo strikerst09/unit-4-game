63fe53c89af3d0aa1a91233b0ed839725c93f8b5
strikerst09 <andrewdehaas5988@gmail.com>
2018-09-13 17:45:26 -0500

    hooked it up with the css and got the number values to randomize at start of game game is complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,9 +11,17 @@ var wins = 0;
 var losses = 0;
 
 // Function for setting crystal value
-document.onload() function() {
-    var crystalValue = 
-}
+function crystalValue() {
+    var crystalValue1 = Math.floor(Math.random() * 3) + 1;
+    var crystalValue2 = Math.floor(Math.random() * 3) + 4;
+    var crystalValue3 = Math.floor(Math.random() * 3) + 7;
+    var crystalValue4 = Math.floor(Math.random() * 3) + 10;
+    $("#crystal1").attr("data-crystalvalue", crystalValue1);
+    $("#crystal2").attr("data-crystalvalue", crystalValue2);
+    $("#crystal3").attr("data-crystalvalue", crystalValue3);
+    $("#crystal4").attr("data-crystalvalue", crystalValue4);
+};
+window.onload = crystalValue();
 
 // Function for on click event
 $(".crystal-image").on("click", function() {
@@ -23,11 +31,11 @@ $(".crystal-image").on("click", function() {
     $("#userNumberBox").text("Your current number is: " + counter); 
     if (counter === targetNumber) {
         wins++;
-        $("#totalsBox").text("You win! Total Wins: " + wins + " Total Losses: " + losses)   
+        $("#totalsBox").text("You win! Total Wins: " + wins + " | Total Losses: " + losses)   
     }
     else if (counter >= targetNumber) {
         losses++;
-        $("#totalsBox").text("You LOSE! Total Wins: " + wins + " Total Losses: " + losses);
+        $("#totalsBox").text("You LOSE! Total Wins: " + wins + " | Total Losses: " + losses);
     }
 });
 
@@ -37,4 +45,5 @@ $("#reset").on("click", function() {
     targetNumber = Math.floor(Math.random() * 101) + 19;
     $("#randomNumberBox").text("Your target is: " + targetNumber);
     $("#userNumberBox").text("Your current number is: " + counter);
+    crystalValue();
 });
